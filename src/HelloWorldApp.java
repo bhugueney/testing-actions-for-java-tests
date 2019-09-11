ad8e18e79cebc8484d1c4363b95c8a3428266b0c
scientific-coder <scientific-coder@bernard-hugueney.org>
2019-09-11 11:43:47 +0200

    HelloWorldApp to build and run

---
+++ src/HelloWorldApp.java
@@ -0,0 +1,8 @@
+public class HelloWorldApp {
+    public static void main(String[] args){
+	System.out.println(greetings("World"));
+    }
+    public static String greetings(String name){
+	return "Hello " + name + " !";
+    }
+}
